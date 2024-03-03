f169dbd7f171927a86d59da44a6bbb51271094e0
White1Reaper <113230473+White1Reaper@users.noreply.github.com>
2024-03-03 20:00:18 +0300

    Задание 2.
    Выполнено и добавлено задание 2.

--- labs/lab6.py
+++ labs/lab6.py
@@ -6,5 +6,31 @@
 import sqlite3
 
 insurance_company = sqlite3.connect('insurance-company.db')
+# Эадание 2. БД должна содержать не менее трех связанных таблиц.
+cursor = insurance_company.cursor()
 
+cursor.execute("""CREATE TABLE IF NOT EXISTS Policies (
+policy_id INTEGER PRIMARY KEY,
+policy_number TEXT NOT NULL,
+start_date TEXT,
+end_date TEXT,
+price REAL
+)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS Clients (
+client_id INTEGER PRIMARY KEY,
+full_name TEXT NOT NULL,
+date_of_birth TEXT,
+address TEXT
+)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS Claims (
+claim_id INTEGER PRIMARY KEY,
+policy_id INTEGER NOT NULL,
+client_id INTEGER NOT NULL,
+claim_date TEXT,
+amount REAL,
+FOREIGN KEY (policy_id) REFERENCES Policies(policy_id),
+FOREIGN KEY (client_id) REFERENCES Clients(client_id)
+)""")
 insurance_company.close()
