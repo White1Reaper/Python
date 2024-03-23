71419b478ae7f8089d63480681bc9091c5712898
White1Reaper <113230473+White1Reaper@users.noreply.github.com>
2024-03-23 18:56:07 +0300

    Задание 2.
    Выполнено и добавлено задание 2.

--- labs/lab7/mysite/app1/models.py
+++ labs/lab7/mysite/app1/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
-# Create your models here.
+#Задание 2. Приложение должно содержать не менее трех связанных моделей (таблиц).
+class Client(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+class Policy(models.Model):
+    policy_number = models.CharField(max_length=50)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    customer = models.ForeignKey(Client, on_delete=models.CASCADE)
+class Claim(models.Model):
+    claim_number = models.CharField(max_length=50)
+    date_filed = models.DateField()
+    description = models.TextField()
+    policy = models.ForeignKey(Policy, on_delete=models.CASCADE)
