cf7115c58c354ed9e9b920fe22ee7f15ba4fb04e
White1Reaper <113230473+White1Reaper@users.noreply.github.com>
2024-03-24 18:03:27 +0300

    Задание 3.
    Выполнено и добавлено задание 3.

--- labs/lab7/mysite/app1/admin.py
+++ labs/lab7/mysite/app1/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Client, Policy, Claim
 
-# Register your models here.
+# Задание 3. Построить отображение моделей в панели администратора, используя различные списки отображения и фильтры.
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+
+class PolicyAdmin(admin.ModelAdmin):
+    list_display = ('policy_number', 'start_date', 'end_date', 'customer')
+    list_filter = ('start_date', 'end_date')
+
+class ClaimAdmin(admin.ModelAdmin):
+    list_display = ('claim_number', 'date_filed', 'description', 'policy')
+    list_filter = ('date_filed',)
+
+# Регистрируем классы ModelAdmin в административной панели
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Policy, PolicyAdmin)
+admin.site.register(Claim, ClaimAdmin)
--- labs/lab7/mysite/app1/models.py
+++ labs/lab7/mysite/app1/models.py
@@ -1,16 +1,29 @@
 from django.db import models
 #Задание 2. Приложение должно содержать не менее трех связанных моделей (таблиц).
+
+
 class Client(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 class Policy(models.Model):
     policy_number = models.CharField(max_length=50)
     start_date = models.DateField()
     end_date = models.DateField()
     customer = models.ForeignKey(Client, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.policy_number
+
 class Claim(models.Model):
     claim_number = models.CharField(max_length=50)
     date_filed = models.DateField()
     description = models.TextField()
     policy = models.ForeignKey(Policy, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.claim_number
